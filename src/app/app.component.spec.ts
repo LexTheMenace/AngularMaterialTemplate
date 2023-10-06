6f0f9f7f21c44b3c1e8a65b4a542c0140effb251
Lex <64288254+LexTheMenace@users.noreply.github.com>
2023-10-06 18:09:58 -0400

    jest test example

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -3,33 +3,32 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  let fixture: any;
+  let component: any;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
     }).compileComponents();
   });
-
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+  });
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
   });
 
   it(`should have as title 'ng-mat-jest'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('ng-mat-jest');
+    component.items = [{ id: 2 }, { id: 3 }, { id: 1 }];
+    component.sortItems();
+    expect(component.items[0].id).toEqual(1);
   });
 
   it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('ng-mat-jest app is running!');
+    component.items = [{ id: 2 }, { id: 3 }, { id: 1 }];
+    component.sortItems('desc');
+    expect(component.items[0].id).toEqual(3);
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'ng-mat-jest';
+  items: { id: number }[] = [{ id: 2 }, { id: 3 }, { id: 1 }];
+
+  sortItems(direction: string = 'asc') {
+    let sorted = this.items.sort((a, b) => (a.id > b.id ? 1 : -1));
+    this.items = direction === 'asc' ? sorted : sorted.reverse();
+  }
 }
